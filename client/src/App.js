cdf791ad34a7e3cc0facf0912cdd9068d8e0c3f1
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-07-22 14:20:57 +0530

    refactor: Remove redundant header and App.css import from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ProductList from './ProductList'; // Import the new ProductList component
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Product App</h1>
-        <ProductList /> {/* Render the ProductList component */}
-      </header>
+      <ProductList /> {/* Render the ProductList component */}
     </div>
   );
 }
